8a0fdc72c093ae5c5b8f60351cc6173d5c04d0f1
jhk <mallangyi@naver.com>
2020-11-15 03:17:12 +0900

    MeetingRoom Algorithm clear1

--- src/MeetingRoom.java
+++ src/MeetingRoom.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MeetingRoom {
@@ -10,24 +12,46 @@ public class MeetingRoom {
         int start ;
         int end   ;
 
+
         int[][] array = new int[size][2];
+
 
         for( int i = 0 ; i < size ; i++){
 
             start   = scanner.nextInt();
             end     = scanner.nextInt();
 
-                array[i][0] = start;
-                array[i][1] = end;
+            array[i][0] = start;
+            array[i][1] = end;
+
+
         }
 
+        Arrays.sort(array, ((o1, o2) -> {
+            if (o1[0] == o2[0]){
+                return Integer.compare(o1[1], o2[1]);
+            }else{
+                return Integer.compare(o1[0], o2[0]);
+            }
+        }));
 
+        int x = 0;
+        while(true){
 
+            if(x >= size ){
+                System.out.println("true");
+                break;
+            }
 
-        System.out.println("어레이는="+array[1][1] );
+            if(array[0][1] >array[x][0]){
+                System.out.println("false");
+                break;
+            }
 
+            x++;
 
-
+        }
+        //System.out.println(array[0][0]);
 
     }
 }
